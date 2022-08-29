c3771b2613be895488f0537a22f2973172fd3db9
Yuriy Berezskyy <54465428+yuriyberezskyy@users.noreply.github.com>
2022-08-29 20:34:41 +0000

    d

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def format_name(f_name,l_name):
+  """Take a first and last name nad format it to return the title case version of name."""
   f_name = f_name.title()
   l_name = l_name.title()
   return f"{f_name} {l_name}"
